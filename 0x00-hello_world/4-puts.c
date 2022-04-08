9ab733dfb21e097eab3ab9e570b62f4f6812d9a2
Olaitan <balogunola195@gmail.com>
2022-04-07 22:05:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - function begins
-*
-* Return: function ends
+* Description: main - Prints out a string to stout.
+* Return: 0 if success.
 */
 int main(void)
 {
-puts("\ "Programming is like building a multilingual puzzle") ;
-return (0) ;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
