dbef2342322dbcfd965ebdf7735029aead251359
Olaitan <balogunola195@gmail.com>
2022-04-07 18:07:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main function begins
+* main - function begins
 *
 * Return: function ends
 */
 int main(void)
 {
-puts('\"Programming is like buildin a multilingual puzzle"');
-return (0);
+puts("\ "Programming is like building a multilingual puzzle") ;
+return (0) ;
 }
