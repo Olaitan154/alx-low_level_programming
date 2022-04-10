1d3b310fcb8eeaf926836726c27eb81fe4387c6b
Olaitan <balogunola195@gmail.com>
2022-04-10 14:39:27 -0700

    second commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 /**
-* Description: main - program that prints all the numbers
+* Description: main - program that prints all the numbers of
+* base 16 in lowercase
+* You can only use the putchar function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 48;
+int l = 'a';
+while (n <= 57)
+{
+putchar (n);
+n += 1;
+}
+while (l <= 'f')
+{
+putchar (l);
+l++;
+}
+putchar ('\n');
+return (0);
+}
