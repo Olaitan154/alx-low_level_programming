c567db85a933a604a9e907066f52e6afa600821a
Olaitan <balogunola195@gmail.com>
2022-04-12 16:31:36 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,19 @@
 * Return: Always 0.
 */
 
-int_abs(int n)
+int _abs(int n)
 {
 
 if (n < 0)
 {
-return (n * (-1));
+return ( n * (-1));
 }
 
 else if (n == 0)
 {
 return (0);
 }
+
 else
 {
 return (n);
