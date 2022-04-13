e3be9ede452b0c061fce1297afa09d5421f91b79
Olaitan <balogunola195@gmail.com>
2022-04-13 03:47:10 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
-* print_alphabet - Entry point
-* Return: 0
+* print_alphabet - print alphabet lowercase
+*
+* Return: Always 0.
 */
+
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
-(
-_putchar (c);
-c++;
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar (alphabet);
 }
+
 _putchar ('\n');
+
 }
