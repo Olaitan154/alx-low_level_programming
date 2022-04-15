f395565be080cf0afa688c0cd6c09b16bbaf010e
Olaitan <balogunola195@gmail.com>
2022-04-15 05:11:49 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+* mul - multiplies two integers
+* @a: multiplying
+* @b: multiplier
+*
+* Return: the result of multiplication
+*/
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+return (result);
+}
