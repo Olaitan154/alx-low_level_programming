dd73453c37b6d28553e4ced8ea0b72034a98bef2
Olaitan <balogunola195@gmail.com>
2022-04-15 06:11:01 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+* print_line - Entry point
+* @n: number of time to print '_'
+* Return: Always 0.
+*/
+void ptint_line(int n)
+{
+int i;
+char k;
+
+k = '_';
+for (i = 0; i < n; i++)
+{
+if (n > o)
+_putchar (k);
+}
+_putchar ('\n');
+}
