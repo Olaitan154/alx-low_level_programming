eb2c05d5e59c619b98a6ff6f4b64f118a787f604
Balogun moshood olaitan <101404208+Olaitan154@users.noreply.github.com>
2022-04-24 22:27:19 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 * reverse_array - reverses an array.
-* @n: an array of integers.
+* @a: an array of integers.
 * @n: number of elements of an array
 *
 * Return: nothing
@@ -13,7 +13,7 @@ int temp;
 
 i = 0;
 n = n - 1;
-
+  
 while (i < n)
 {
 temp = a[i];
