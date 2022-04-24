a77269e8e00537ca6eec48b569efeefbcfc23f13
Olaitan <balogunola195@gmail.com>
2022-04-24 02:13:33 -0700

    More_pointers_arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* _strncat - function that concatenates two strings.
+* _strncat - function thst concanates two strings.
 *
 * @dest: pointer to destination char
 * @src: pointer to source char
@@ -16,9 +16,9 @@ int j = 0;
 
 while (*(dest + i) != '\0')
 i++;
-while(*(src + j) != '\0')
+while (*(src + j) != '\0')
 {
-if  (j < n)
+if (j < n)
 {
 *(dest + i) = *(src + j);
 i++;
