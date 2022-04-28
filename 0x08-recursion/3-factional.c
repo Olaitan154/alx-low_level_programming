23fbbf162b267be22f1e49e4988d9cc68b1f468a
Balogun moshood olaitan <101404208+Olaitan154@users.noreply.github.com>
2022-04-28 22:42:50 +0100

    Recursion

--- 0x08-recursion/3-factional.c
+++ 0x08-recursion/3-factional.c
@@ -1,19 +1,18 @@
 #include "holberton.h"
 
-?88
-* factional - returns the factional of a given number.
-*
-*
-* @n: input number
-*
-* Return: int
-*/
+/**
+ * factorial - returns the factorial of a given number.
+ *
+ * @n: input number
+ *
+ * Return: int
+ */
 
-int factional(int n)
+int factorial(int n)
 {
-if (n < 0)
-return (-1);
-if (n == 0)
-return (1);
-return (n * factional(n - 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
